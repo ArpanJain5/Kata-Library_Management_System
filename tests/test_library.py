ee97fa1c8488d3415e6821fcd78124d840a813bb
ArpanJain5 <arpanrewari@gmail.com>
2024-10-25 13:16:16 +0530

    Add Test for adding books

--- tests/test_library.py
+++ tests/test_library.py
@@ -1,13 +1,10 @@
 import unittest
-from library import Library
+from library_management.library import Library  # Import the Library class from the main code
 class TestLibrary(unittest.TestCase):
-    
+
     def test_add_book(self):
         """Test adding a book to the library."""
         library = Library()
-        library.add_book(1, "The Jungle Book", "Rudyard Kipling", 3)
+        library.add_book(1,"The Jungle Book", "Rudyard Kipling", 3)
         self.assertIn(1, library.books)  # Check if the book is added
         self.assertEqual(library.books[1], ["The Jungle Book", "Rudyard Kipling", 3])  # Verify book details
-
-if __name__ == "__main__":
-   unittest.main()
